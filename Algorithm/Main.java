48512d713dc739bdcfcffa3c90655435d210bc00
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-11 18:11:08 +0200

    isPalindrome Java

---
+++ Algorithm/Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        
+        String word = "Potop";
+        boolean palindrome = Palindrome.isPalindrome(word);
+        System.out.println(palindrome);
+    }
+}
---
+++ Algorithm/Palindrome.java
@@ -0,0 +1,22 @@
+public class Palindrome {
+    
+    public static boolean isPalindrome(String text){
+        String reserved = "";
+
+        for (int i = text.length() - 1; i >= 0; i--){
+            reserved += text.charAt(i);
+        }
+        
+        String palOne = text.toUpperCase();
+        String palTwo = reserved.toUpperCase();
+    
+        if (palOne.equals(palTwo))
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+}
