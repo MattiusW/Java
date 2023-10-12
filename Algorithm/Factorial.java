833bf5965028141db278ddc2d8a6f5fe9a8f7e13
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-12 13:59:50 +0200

    Correct and push

--- Algorithm/Factorial.java
+++ Algorithm/Factorial.java
@@ -3,8 +3,8 @@ package Algorithm;
 public class Factorial{
     
     public static int factorialAmount(int number){
-        
         int result = 1; 
+        
         for (int i = 1; i <= number; i++)
         {
             result *= i; 
