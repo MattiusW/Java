faa2aac8f319357513026d0c4e4119d5dca9182c
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-12 14:55:43 +0200

    Import arrays and change object tab to String

--- Algorithm/Main.java
+++ Algorithm/Main.java
@@ -1,12 +1,13 @@
 package Algorithm;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 class Main{
     public static void main(String[] args){
         int[] tab = {1,1,2,2,3,3,4,4,5,5};
         ArrayList<Integer> fib = Fibonacci.fibonacciSequence(tab);
-        System.out.println("Tab: " + tab + "Fibonacci: " + fib);
+        System.out.println("Tab: " + Arrays.toString(tab) + " Fibonacci: " + fib);
         int factorial = Factorial.factorialAmount(5);
         System.out.println("Factorial: " + factorial);
     }
