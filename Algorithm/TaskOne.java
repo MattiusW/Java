e488aa66ca7d3f7df73f2895a80cbaf28754b05e
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-12 14:55:43 +0200

    Reserve String

---
+++ Algorithm/TaskOne.java
@@ -0,0 +1,19 @@
+package Algorithm;
+
+class TaskOne{
+    public static void main(String[] args){
+        String example = "Hello World";
+        String reserved = reservedWord(example);
+        System.out.println(reserved);
+    }
+
+    public static String reservedWord(String word){
+        String reserved = ""; 
+
+        for(int i = word.length() - 1; i >= 0; i--){
+            reserved += word.charAt(i);
+        }
+
+        return reserved;
+    }
+}
