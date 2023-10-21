953cdded90668a7eb7ca2802f0316a54f785b287
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-21 17:07:54 +0200

    Reserve add number to table

---
+++ Spoj/ReservedNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+class ReservedNumbers{
+    
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many number you enter?: ");
+        int howMany = scan.nextInt();
+        int[] result = reserveTable(howMany);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] reserveTable(int n){
+        int[] table = new int[n];
+        Scanner scanner = new Scanner(System.in);
+            
+        for(int i = table.length - 1; i >= 0; i--){
+            int number = scanner.nextInt();
+            table[i] += number;
+        }
+
+        return table;
+    }
+        
+}
