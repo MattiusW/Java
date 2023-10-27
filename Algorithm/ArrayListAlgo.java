e4d3cb50759f51be7d31597c0302b109ba0c688a
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-27 09:45:48 +0200

    Operation in list

--- Algorithm/ArrayListAlgo.java
+++ Algorithm/ArrayListAlgo.java
@@ -1,7 +1,28 @@
 package Algorithm;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListAlgo {
     
+    public static void main(String[] args){
+        List<String> example = new ArrayList<String>();
+        example.add("One");
+        example.add("Two");
+        example.add("Three");
+        example.add("Four");
+        example.add("Five");
+
+        System.out.println(example);
+
+        System.out.println(example.get(3));
+        
+        example.remove(3);
+        
+        System.out.println(example.get(3));
+
+        System.out.println(example);
+
+    }
+
 }
