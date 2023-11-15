da1ae465c4ad4c158c63f9c37abe07cdf028d000
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-15 10:50:37 +0100

    LeetCode: MovedZero

---
+++ LeetCode/MovedZero.java
@@ -0,0 +1,16 @@
+package LeetCode;
+
+// Mając tablicę liczb całkowitych nums, 
+// przesuń wszystkie 0'' na koniec, zachowując względną kolejność niezerowych elementów.
+
+public class MovedZero {
+    
+    public static void main(String[] args){
+
+    }
+
+    public static void moveZeroes(int[] nums){
+        
+    }
+
+}
