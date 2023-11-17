ae1b878f44f8284c5cc447189b1f073f1631e38d
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-17 09:28:31 +0100

    Contains duplicate

---
+++ LeetCode/ContainsDuplicate.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class ContainsDuplicate {
+    
+    public static void main(String[] args){
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+        
+    }
+
+    public static boolean containsDuplicate(int[] nums){
+        for(int i = 1; i < nums.length; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+}
