cd78f3869a88830a79fd7082fc9365b8745fe6f0
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-15 20:33:57 +0100

    Solve

--- LeetCode/String/CountItemsMatchingRule.java
+++ LeetCode/String/CountItemsMatchingRule.java
@@ -5,7 +5,7 @@ public class CountItemsMatchingRule {
     public static void main(String[] args){
         List<List<String>> items = new ArrayList<>();
         String ruleKey = "color";
-        String ruleValue = "phone";
+        String ruleValue = "sliver";
 
         List<String> listOne = new ArrayList<>();
         listOne.add("phone");
@@ -33,13 +33,12 @@ public class CountItemsMatchingRule {
 
     public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int count = 0;
-        Integer.parseInt(ruleKey);
+        String[] rule = {"type", "color", "name"}; 
 
         for(int i = 0; i < items.size(); i++){
             for(int j = 0; j < items.get(i).size(); j++){
-                if(items.get(i).get(j).equals(ruleValue)){
+                if(items.get(i).get(j).equals(ruleValue) && rule[j].equals(ruleKey)){
                     count++;
-                    break;
                 }
             }
         }
