60d17c0b5ba2e4731a7e62a67a3a046386a19d99
Mateusz Walczyk <mattiusw@gmail.com>
2023-12-15 20:20:57 +0100

    Not end

--- LeetCode/String/CountItemsMatchingRule.java
+++ LeetCode/String/CountItemsMatchingRule.java
@@ -4,6 +4,8 @@ import java.util.List;
 public class CountItemsMatchingRule {
     public static void main(String[] args){
         List<List<String>> items = new ArrayList<>();
+        String ruleKey = "color";
+        String ruleValue = "phone";
 
         List<String> listOne = new ArrayList<>();
         listOne.add("phone");
@@ -25,6 +27,22 @@ public class CountItemsMatchingRule {
         items.add(listThree);
 
         System.out.println(items);
+        System.out.println(countMatches(items, ruleKey, ruleValue));
         
+    }
+
+    public static int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        Integer.parseInt(ruleKey);
+
+        for(int i = 0; i < items.size(); i++){
+            for(int j = 0; j < items.get(i).size(); j++){
+                if(items.get(i).get(j).equals(ruleValue)){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
     }
 }
