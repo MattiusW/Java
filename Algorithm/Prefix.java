767e619c298a1e2d80efff68e8cbba4beb44f0c2
Mateusz Walczyk <mattiusw@gmail.com>
2024-01-09 07:59:38 +0100

    Out of range protection

--- Algorithm/Prefix.java
+++ Algorithm/Prefix.java
@@ -4,7 +4,7 @@ class Prefix {
 
     public static void main(String[] args) {
         int[] table = {2,2,3,1,1,2,1,4,3,1,2,3,2};
-        System.out.println(prefix(table, 2, 5));
+        System.out.println(prefix(table, 0, 5));
     }
 
     public static int prefix(int[] table, int l, int r) {
@@ -18,7 +18,7 @@ class Prefix {
         System.out.print("Pref table: ");
         System.out.println(Arrays.toString(pref));
 
-        if (l == 1){
+        if (l == 1 || l == 0){
             sum = pref[r];
         }
         else {
